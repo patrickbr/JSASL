a1b62620dd488a4f857b3777a5dd68aafd2d8efa
Patrick Brosi <info@patrickbrosi.de>
2015-02-16 00:01:35 +0100

    add a few more examples

--- example/script.js
+++ example/script.js
@@ -93,7 +93,7 @@ function load(what) {
       str = 'def take n l = if n=0 or l=nil then nil\nelse x:take (n-1) xs where x = hd l;\nxs = tl l\ndef mod x y = (x - (x/y)*y)\ndef primes = sieve (naturals 2)\n\ndef sieve input = (hd input) : (sieve (removeFromList (tl input) (hd input)))\n\ndef removeFromList list ele =if (mod (hd list) ele) = 0\nthen (removeFromList (tl list) ele)\n                        else (hd list) : (removeFromList (tl list) ele)\n\ndef naturals x = x : (naturals (x+1))\n\n.\n\ntake 20 primes';
       break;
     case 'funcInLine':
-      str = 'def id x = x\ndef until p f x = if p x then x else until p f (f x)\ndef comp f g x = f (g x)\ndef map f l = if l=nil then nil\nelse (f x):(map f xs) where x = hd l;\n				xs = tl l\ndef fold m z l = if l=nil then z			\n	else m x (fold m z xs) where x = hd l;	\n					 xs = tl l\ndef append l1 l2 = if l1=nil then l2			\n	else x:(append xs l2) where x = hd l1;		\n					xs = tl l1\ndef reverse l = if l=nil then nil		\n	else append (reverse (tl l)) ((hd l):nil)		\ndef filter p l = if l=nil then nil\n\n	else (if p x then x:(filter p xs)	\n	else filter p xs) where x = hd l;			\n			   xs = tl l	\ndef sort p l = if l=nil then nil			\n	else insert p (hd l) (sort p (tl l))			\n	where				\n	insert pp e ll = if ll=nil then (e:nil)		\n		else			\n		if pp e (hd ll) then (e:ll)		\n		else			\n		((hd ll):(insert pp e (tl ll)))	\ndef drop n l = if n<=0 then l				\n	else if l=nil then nil	\n	else drop (n-1) (tl l)		\ndef take n l = if n=0 or l=nil then nil		\n	else x:take (n-1) xs where x = hd l;		\n				   xs = tl l	\ndef at n l = if n=0 then hd l				\n	else at (n-1) (tl l)	\ndef null l = l=nil			\ndef length l = if l=nil then 0			\n	else 1+(length(tl l))			\ndef sum = fold plus 0		\ndef product = fold times 1			\ndef plus x y = x+y	\ndef mul x y = x*y			\ndef div x y = x/y			\ndef div2 y x = y/x\ndef minus x y = x-y\ndef minus2 y x = y-x\ndef lt x y = x<y		\ndef leq x y = x<=y				\ndef eq x y = x=y				\ndef geq x y = x>=y	\ndef gt x y = x>y\ndef mod x y = (x - (x/y)*y)\n.\nat 0 a (at 1 a 4 1) 2 where a = [plus,minus]';
+      str = 'def at n l = if n=0 then hd l\nelse at (n-1) (tl l)	\ndef plus x y = x+y	\ndef minus x y = x-y\n.\nat 0 a (at 1 a 4 1) 2 where a = [plus,minus]'
       break;
     case 'dropTake':
       str = 'def drop n l = if n<=0 then l		\n	else if l=nil then nil				\n	else drop (n-1) (tl l)	\ndef take n l = if n=0 or l=nil then nil			\n	else x:take (n-1) xs where x = hd l;	\n				   xs = tl l\n\n  def append l1 l2 = if l1=nil then l2			\n	else x:(append xs l2) where x = hd l1;		\n					xs = tl l1				   \n\ndef list = [1,2,3,4,5,6,7,8,9,10]\n.\n\nappend (take 4 list) (drop 4 list)';
@@ -131,6 +131,19 @@ function load(what) {
     case 'stringimplode':
       str = 'def strimp_h strl tmp = if strl = nil then tmp else strimp_h (tl strl) (tmp + (hd strl))\ndef strimp strl = strimp_h strl ""\n.\nstrimp ["a", "b", "c"]';
       break;
+    case 'fold':
+      str = 'def fold m z l = if l=nil then z\n  else m x (fold m z xs) where x = hd l;	xs = tl l\ndef plus a b = a + b\n.\nfold plus 0 [1,2,3,4,5]';
+      break;
+    case 'filter':
+      str = 'def take n l = if n=0 or l=nil then nil\nelse x:take (n-1) xs where x = hd l;\nxs = tl l\ndef naturals n = n : (naturals (n+1))\ndef filter p l = if l=nil then nil\n	else (if p x then x:(filter p xs)	\n	else filter p xs) where x = hd l;	\n			            xs = tl l	\ndef mod x y = (x - (x/y)*y)\ndef evens n = filter p (naturals n) where p a = mod a 2 = 0\n.\ntake 100 (evens 1)';
+      break;
+    case 'map':
+      str = 'def map f l = if l=nil then nil\n  else (f x):(map f xs) where x = hd l;\n				  xs = tl l\ndef naturals x = x : (naturals (x+1))\ndef mod x y = (x - (x/y)*y)\n.\nmap f (naturals 0) where f n = mod n 2 \n';
+      break;
+    case 'fibonacci':
+      str = 'def fibonacci n = if n < 3 then 1 else (fibonacci (n-1)) + (fibonacci (n-2))\ndef fibonaccis n = fibonacci n : fibonaccis(n+1)\ndef take n l = if n=0 or l=nil then nil	\n	else x:take (n-1) xs where x = hd l;		\n				   xs = tl l	\n.\ntake 15 (fibonaccis 1)';
+      break;
   }
-  document.getElementById('texta').value = str;
+  var texta = document.getElementById('texta');
+  if (texta) texta.value = str;
 }
